45e0f4962c202948877f61f701f11417cabf6107
Sabin Khatri <khatrisabin611@gmail.com>
2024-02-20 08:57:00 +0545

    calculator in HTML CSS and JS

---
+++ app.js
@@ -0,0 +1,30 @@
+let inputbox = document.getElementById('inputbox')
+let buttons = document.querySelectorAll('button')
+
+let string = ''
+
+buttons.forEach(Element =>{
+    Element.addEventListener('click', (b) => {
+        if (b.target.innerText == '=') {
+            string = String(eval(string))
+            inputbox.value = string;
+        }
+        else if (b.target.innerText == 'AC') {
+            string = ''
+            inputbox.value = string;
+        }
+        else if (b.target.innerText == 'DEL') {
+            string = string.substring(0,string.length-1)
+            inputbox.value = string;
+        }
+        else if (b.target.id == '+/-') {
+            string = String(-eval(string));
+
+            inputbox.value = string;
+        }
+        else {
+            string += b.target.innerText
+            inputbox.value = string
+        }
+    });
+});
